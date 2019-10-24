8c5a6aaaf84de596365b14043d6d2c5e57c92359
Qiushi Pan <qiu.gits@gmail.com>
2019-10-24 11:04:45 +0900

    Add: summary reports

---
+++ tmp_experiments.sh
@@ -0,0 +1,4 @@
+ python main.py --config config/1023_adjust_seqlen_NEL.ini
+ python main.py --config config/1023_adjust_seqlen_NER.ini
+ python main.py --config config/1023_adjust_seqlen_NES.ini
+ python main.py --config config/1023_adjust_seqlen_NEW.ini
