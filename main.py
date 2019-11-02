52ffa5e8eb8fa0711b25509b742f56565a97d71b
Qiushi Pan <qiu.gits@gmail.com>
2019-11-03 03:35:11 +0900

    Refactor: separate save functions

--- main.py
+++ main.py
@@ -22,47 +22,12 @@ import matplotlib.pyplot as plt
 
 from src.utils import sAsMinutes, timeSince
 from src.config import get_option_fallback, Config
+from src.save import save_model, save_log, save_hm_fig, save_learning_curve
 from knowledge_tracing.trainer import Trainer
 
 logging.basicConfig()
 logger = logging.getLogger(__file__)
 logger.setLevel(logging.INFO)
-
-
-def save_model(config, model, auc, epoch):
-    checkpointsdir = config.resultsdir / 'checkpoints'
-    checkpointsdir.mkdir(exist_ok=True)
-    torch.save(model.state_dict(), checkpointsdir /
-               f'{config.model_name}_auc{auc:.4f}_e{epoch}.model')
-
-
-def save_log(config, data, auc, epoch):
-    lc_datadir = config.resultsdir / 'lc_data'
-    lc_datadir.mkdir(exist_ok=True)
-    with open(lc_datadir / f'{config.model_name}_auc{auc:.4f}_e{epoch}.pickle', 'wb') as f:
-        pickle.dump(data, f)
-
-
-def save_hm_fig(config, sns_fig):
-    hmdir = config.resultsdir / 'heatmap'
-    hmdir.mkdir(exist_ok=True)
-    sns_fig.savefig(hmdir / f'{config.model_name}.png')
-
-
-def save_learning_curve(x, train_loss_list, train_auc_list, eval_loss_list, eval_auc_list, config):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    if train_loss_list:
-        ax.plot(x, train_loss_list, label='train loss')
-    if train_auc_list:
-        ax.plot(x, train_auc_list, label='train auc')
-    if eval_loss_list:
-        ax.plot(x, eval_loss_list, label='eval loss')
-    ax.plot(x, eval_auc_list, label='eval auc')
-    ax.legend()
-    print(len(train_loss_list), len(eval_loss_list), len(eval_auc_list))
-    lcdir = config.resultsdir / 'learning_curve'
-    plt.savefig(lcdir / f'{config.model_name}.png')
 
 
 def main(configpath: Path):
