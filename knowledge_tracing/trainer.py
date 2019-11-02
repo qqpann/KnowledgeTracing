a6714d58867513b12337b6ff5acb75de1a1a885b
Qiushi Pan <qiu.gits@gmail.com>
2019-11-03 03:25:56 +0900

    Refactor: move training core to Trainer

--- knowledge_tracing/trainer.py
+++ knowledge_tracing/trainer.py
@@ -1,14 +1,113 @@
+import logging
 import numpy as np
+from math import log, ceil
+
+import torch
+
+from src.data import prepare_data
+from model.eddkt import EncDecDKT, get_loss_batch_encdec
+from model.basedkt import BaseDKT, get_loss_batch_basedkt
+from model.seq2seq import get_Seq2Seq, get_loss_batch_seq2seq
+
 
 class Trainer(object):
-    def __init__(self, config, model, loss_batch, logger, loss, opt, train_dl):
+    def __init__(self, config):
         self.config = config
-        self.model = model
-        self.loss_batch = loss_batch
-        self.logger = logger
-        self.loss = loss
-        self.opt = opt
-        self.train_dl = train_dl
+        self.logger = self.get_logger()
+        self.device = self.get_device()
+        self.model, self.loss_batch, self.train_dl, _eval_dl = self.get_model()
+        self.opt = self.get_opt(self.model)
+
+    def get_logger(self):
+        logging.basicConfig()
+        logger = logging.getLogger(self.config.model_name)
+        logger.setLevel(logging.INFO)
+        return logger
+
+    def get_device(self):
+        self.logger.info('PyTorch: {}'.format(torch.__version__))
+        device = torch.device(
+            'cuda' if self.config.cuda and torch.cuda.is_available() else 'cpu')
+        self.logger.info('Using Device: {}'.format(device))
+        return device
+
+    def get_model(self):
+        # =========================
+        # Parameters
+        # =========================
+        batch_size = self.config.batch_size
+        n_hidden, n_skills, n_layers = 200, self.config.n_skills, 2
+        n_output = n_skills
+        PRESERVED_TOKENS = 2  # PAD, SOS
+        onehot_size = 2 * n_skills + PRESERVED_TOKENS
+        n_input = ceil(log(2 * n_skills))
+
+        INPUT_DIM, ENC_EMB_DIM, ENC_DROPOUT = onehot_size, n_input, 0.6
+        OUTPUT_DIM, DEC_EMB_DIM, DEC_DROPOUT = onehot_size, n_input, 0.6
+        HID_DIM, N_LAYERS = n_hidden, n_layers
+        N_SKILLS = n_skills
+        # =========================
+        # Prepare models, LossBatch, and Data
+        # =========================
+        if self.config.model_name == 'encdec':
+            model = EncDecDKT(
+                INPUT_DIM, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT,
+                OUTPUT_DIM, DEC_EMB_DIM, HID_DIM, N_LAYERS, DEC_DROPOUT,
+                N_SKILLS,
+                self.device).to(self.device)
+            loss_batch = get_loss_batch_encdec(
+                self.config.extend_forward, ks_loss=self.config.ks_loss)
+            train_dl, eval_dl = prepare_data(
+                self.config.source_data, 'encdec', n_skills, PRESERVED_TOKENS,
+                min_n=3, max_n=self.config.sequence_size, batch_size=batch_size, device=self.device, sliding_window=0,
+                params={'extend_backward': self.config.extend_backward, 'extend_forward': self.config.extend_forward})
+        elif self.config.model_name == 'seq2seq':
+            model = get_Seq2Seq(
+                onehot_size, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT,
+                OUTPUT_DIM, DEC_EMB_DIM, DEC_DROPOUT, self.device)
+            loss_batch = get_loss_batch_seq2seq(
+                self.config.extend_forward, ks_loss=self.config.ks_loss)
+            train_dl, eval_dl = prepare_data(
+                self.config.source_data, 'encdec', n_skills, PRESERVED_TOKENS,
+                min_n=3, max_n=self.config.sequence_size, batch_size=batch_size, device=self.device, sliding_window=0,
+                params={'extend_backward': self.config.extend_backward, 'extend_forward': self.config.extend_forward})
+
+        elif self.config.model_name == 'basernn':
+            model = BaseDKT(
+                self.device, self.config.model_name, n_input, n_hidden, n_output, n_layers, batch_size
+            ).to(self.device)
+            loss_batch = get_loss_batch_basedkt(
+                onehot_size, n_input, batch_size, self.config.sequence_size, self.device)
+            train_dl, eval_dl = prepare_data(
+                self.config.source_data, 'base', n_skills, preserved_tokens='?',
+                min_n=3, max_n=self.config.sequence_size, batch_size=batch_size, device=self.device, sliding_window=0)
+        elif self.config.model_name == 'baselstm':
+            model = BaseDKT(
+                self.device, self.config.model_name, n_input, n_hidden, n_output, n_layers, batch_size
+            ).to(self.device)
+            loss_batch = get_loss_batch_basedkt(
+                onehot_size, n_input, batch_size, self.config.sequence_size, self.device)
+            train_dl, eval_dl = prepare_data(
+                self.config.source_data, 'base', n_skills, preserved_tokens='?',
+                min_n=3, max_n=self.config.sequence_size, batch_size=batch_size, device=self.device, sliding_window=0)
+        else:
+            raise ValueError(f'model_name {self.config.model_name} is wrong')
+        self.logger.info(
+            'train_dl.dataset size: {}'.format(len(train_dl.dataset)))
+        self.logger.info(
+            'eval_dl.dataset size: {}'.format(len(eval_dl.dataset)))
+
+        def count_parameters(model):
+            return sum(p.numel() for p in model.parameters() if p.requires_grad)
+        self.logger.info(
+            f'The model has {count_parameters(model):,} trainable parameters')
+
+
+        return model, loss_batch, train_dl, eval_dl
+
+    def get_opt(self, model):
+        opt = torch.optim.SGD(model.parameters(), lr=self.config.lr)
+        return opt
 
     def train_model(self):
         train_loss_list = []
@@ -24,7 +123,8 @@ class Trainer(object):
 
             current_epoch_train_loss = []
             for args in self.train_dl:
-                loss = self.loss_batch(self.model, self.loss, *args, opt=self.opt)
+                loss = self.loss_batch(
+                    self.model, *args, opt=self.opt)
                 current_epoch_train_loss.append(loss.item())
 
                 # stop at first batch if debug
@@ -34,5 +134,6 @@ class Trainer(object):
             if print_train:
                 loss_array = np.array(current_epoch_train_loss)
                 if epoch % 100 == 0:
-                    self.logger.info('TRAIN Epoch: {} Loss: {}'.format(epoch, loss_array.mean()))
+                    self.logger.info('TRAIN Epoch: {} Loss: {}'.format(
+                        epoch, loss_array.mean()))
                 train_loss_list.append(loss.mean())
--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import configparser
 import json
 import pickle
 from pathlib import Path
-from math import log, ceil
 from typing import List, Tuple, Set, Dict  # noqa
 
 import numpy as np
@@ -21,16 +20,12 @@ from sklearn import metrics
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from src.data import prepare_data, prepare_heatmap_data, SOURCE_ASSIST0910_SELF, SOURCE_ASSIST0910_ORIG
 from src.utils import sAsMinutes, timeSince
 from src.config import get_option_fallback, Config
-from model.eddkt import EncDecDKT, get_loss_batch_encdec
-from model.basedkt import BaseDKT, get_loss_batch_basedkt
-from model.seq2seq import get_Seq2Seq, get_loss_batch_seq2seq
 from knowledge_tracing.trainer import Trainer
 
 logging.basicConfig()
-logger = logging.getLogger()
+logger = logging.getLogger(__file__)
 logger.setLevel(logging.INFO)
 
 
@@ -90,7 +85,7 @@ def main(configpath: Path):
             'load_model': '',
             'plot_heatmap': False,
             'plot_lc': False,
-            'source_data': SOURCE_ASSIST0910_ORIG,
+            'source_data': 'original_ASSISTmentsSkillBuilder0910',  # SOURCE_ASSIST0910_ORIG,
             'ks_loss': False,
             'extend_backward': 0,
             'extend_forward': 0,
@@ -111,116 +106,26 @@ def main(configpath: Path):
         report = run(config)
         report_list.append(report)
     print(report)
-    with open(projectdir / 'output' / 'reports' / '{}result.json'.format(config._get_stem_name()), 'w') as f:
-        json.dump(report_list, f)
+    if report is not None:
+        with open(projectdir / 'output' / 'reports' / '{}result.json'.format(config._get_stem_name()), 'w') as f:
+            json.dump(report_list, f)
 
 
 def run(config):
     assert config.model_name in {'encdec', 'basernn', 'baselstm', 'seq2seq'}
     report = dict()
-    # =========================
-    # Outfile name
-    # =========================
-    model_fname = config.model_name
-    model_fname += f'eb{config.extend_backward}' if config.extend_backward else ''
-    model_fname += f'ef{config.extend_forward}' if config.extend_forward else ''
-    model_fname += f'ks' if config.ks_loss else ''
-    report['model_fname'] = model_fname
+    report['model_fname'] = config.outfname
 
-    # =========================
-    # Version, Device
-    # =========================
-    print('PyTorch:', torch.__version__)
-    dev = torch.device(
-        'cuda' if config.cuda and torch.cuda.is_available() else 'cpu')
-    print('Using Device:', dev)
+    trainer = Trainer(config)
+    trainer.train_model()
+    return None
 
-    # =========================
-    # Parameters
-    # =========================
-    batch_size = config.batch_size
-    n_hidden, n_skills, n_layers = 200, config.n_skills, 2
-    n_output = n_skills
-    PRESERVED_TOKENS = 2  # PAD, SOS
-    onehot_size = 2 * n_skills + PRESERVED_TOKENS
-    n_input = ceil(log(2 * n_skills))
-
-    INPUT_DIM, ENC_EMB_DIM, ENC_DROPOUT = onehot_size, n_input, 0.6
-    OUTPUT_DIM, DEC_EMB_DIM, DEC_DROPOUT = onehot_size, n_input, 0.6
-    HID_DIM, N_LAYERS = n_hidden, n_layers
-    N_SKILLS = n_skills
-    # OUTPUT_DIM = n_output = 124  # TODO: ほんとはこれやりたい
-
-    # =========================
-    # Prepare models, LossBatch, and Data
-    # =========================
-    if config.model_name == 'encdec':
-        model = EncDecDKT(
-            INPUT_DIM, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT,
-            OUTPUT_DIM, DEC_EMB_DIM, HID_DIM, N_LAYERS, DEC_DROPOUT,
-            N_SKILLS,
-            dev).to(dev)
-        loss_batch = get_loss_batch_encdec(
-            config.extend_forward, ks_loss=config.ks_loss)
-        train_dl, eval_dl = prepare_data(
-            config.source_data, 'encdec', n_skills, PRESERVED_TOKENS,
-            min_n=3, max_n=config.sequence_size, batch_size=batch_size, device=dev, sliding_window=0,
-            params={'extend_backward': config.extend_backward, 'extend_forward': config.extend_forward})
-    elif config.model_name == 'seq2seq':
-        model = get_Seq2Seq(
-            onehot_size, ENC_EMB_DIM, HID_DIM, N_LAYERS, ENC_DROPOUT,
-            OUTPUT_DIM, DEC_EMB_DIM, DEC_DROPOUT, dev)
-        loss_batch = get_loss_batch_seq2seq(
-            config.extend_forward, ks_loss=config.ks_loss)
-        train_dl, eval_dl = prepare_data(
-            config.source_data, 'encdec', n_skills, PRESERVED_TOKENS,
-            min_n=3, max_n=config.sequence_size, batch_size=batch_size, device=dev, sliding_window=0,
-            params={'extend_backward': config.extend_backward, 'extend_forward': config.extend_forward})
-
-    elif config.model_name == 'basernn':
-        model = BaseDKT(
-            dev, config.model_name, n_input, n_hidden, n_output, n_layers, batch_size
-        ).to(dev)
-        loss_batch = get_loss_batch_basedkt(
-            onehot_size, n_input, batch_size, config.sequence_size, dev)
-        train_dl, eval_dl = prepare_data(
-            config.source_data, 'base', n_skills, preserved_tokens='?',
-            min_n=3, max_n=config.sequence_size, batch_size=batch_size, device=dev, sliding_window=0)
-    elif config.model_name == 'baselstm':
-        model = BaseDKT(
-            dev, config.model_name, n_input, n_hidden, n_output, n_layers, batch_size
-        ).to(dev)
-        loss_batch = get_loss_batch_basedkt(
-            onehot_size, n_input, batch_size, config.sequence_size, dev)
-        train_dl, eval_dl = prepare_data(
-            config.source_data, 'base', n_skills, preserved_tokens='?',
-            min_n=3, max_n=config.sequence_size, batch_size=batch_size, device=dev, sliding_window=0)
-    else:
-        raise ValueError(f'model_name {config.model_name} is wrong')
-    logger.info('train_dl.dataset size: {}'.format(len(train_dl.dataset)))
-    logger.info('eval_dl.dataset size: {}'.format(len(eval_dl.dataset)))
-
-    print(model)
-
-    # ========================
-    # Load trained model
-    # ========================
-    if config.load_model:
-        model.load_state_dict(torch.load(config.load_model))
-        model = model.to(dev)
-    else:
-        # -------------------------
-        def count_parameters(model):
-            return sum(p.numel() for p in model.parameters() if p.requires_grad)
-        logger.info(
-            f'The model has {count_parameters(model):,} trainable parameters')
-
-        loss = nn.BCELoss()
-        opt = optim.SGD(model.parameters(), lr=config.lr)
-
-        trainer = Trainer(config, model, loss_batch,
-                          logger, loss, opt, train_dl)
-        trainer.train_model()
+    # # ========================
+    # # Load trained model
+    # # ========================
+    # if config.load_model:
+    #     model.load_state_dict(torch.load(config.load_model))
+    #     model = model.to(dev)
 
     #         # =====
     #         # EVAL
@@ -405,7 +310,6 @@ def run(config):
     #     save_hm_fig(config, fig)
 
     # return report
-    return None
 
 
 if __name__ == '__main__':
