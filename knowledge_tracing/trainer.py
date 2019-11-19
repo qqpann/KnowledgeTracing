f7313a2906addaba9de1a21b4857e258d871e178
Qiushi Pan <qiu.gits@gmail.com>
2019-11-19 17:10:26 +0900

    Update: common in train and eval to exec_core

--- knowledge_tracing/trainer.py
+++ knowledge_tracing/trainer.py
@@ -6,12 +6,14 @@ from math import log, ceil
 from sklearn import metrics
 
 from src.data import prepare_data, prepare_dataloader
+from src.save import save_model, save_log, save_hm_fig, save_learning_curve
 from model.eddkt import EncDecDKT, get_loss_batch_encdec
 from model.basedkt import BaseDKT
 from model.seq2seq import get_Seq2Seq, get_loss_batch_seq2seq
 
 
 class Trainer(object):
+
     def __init__(self, config):
         self.config = config
         self.logger = self.get_logger()
@@ -86,7 +88,8 @@ class Trainer(object):
                 self.config,
                 self.device, self.config.model_name, n_input, n_hidden, n_output, n_layers, batch_size
             ).to(self.device)
-            train_dl, eval_dl = prepare_dataloader(self.config, device=self.device)
+            train_dl, eval_dl = prepare_dataloader(
+                self.config, device=self.device)
         else:
             raise ValueError(f'model_name {self.config.model_name} is wrong')
         self.logger.info(
@@ -105,95 +108,73 @@ class Trainer(object):
         opt = torch.optim.SGD(model.parameters(), lr=self.config.lr)
         return opt
 
+    def train_model_simple(self):
+        '''最小構成を見て基本を思い出す'''
+        for epoch in range(1, self.config.epoch_size + 1):
+            self.model.train()
+            for i, (xseq, yseq) in enumerate(self.train_dl):
+                out = self.model.loss_batch(xseq, yseq, opt=self.opt)
+                loss = out['loss']
+
     def train_model(self, validate=True):
         self.logger.info('Starting train')
-        train_loss_list = []
         bset_eval_auc = 0.
-
         # start_time = time.time()
         for epoch in range(1, self.config.epoch_size + 1):
             self.model.train()
+            t_loss, t_auc = self.exec_core(self.train_dl, self.opt)
 
-            current_epoch_train_loss = []
-            for xseq, yseq in self.train_dl:
-                out = self.model.loss_batch(xseq, yseq, opt=self.opt)
-                loss = out['loss']
-                current_epoch_train_loss.append(loss.item())
+            if epoch % 100 == 0:
+                self.logger.info('\tEpoch {}\tTrain Loss: {:.4}\tAUC: {:.4}'.format(
+                    epoch, t_loss, t_auc))
 
-                # stop at first batch if debug
-                if self.config.debug:
-                    break
-
-            if epoch % 10 == 0:
-                loss_array = np.array(current_epoch_train_loss)
-                if epoch % 100 == 0:
-                    self.logger.info('TRAIN Epoch: {} Loss: {:.4}'.format(
-                        epoch, loss_array.mean()))
-                train_loss_list.append(loss.mean())
-
-            if validate:
-                v_loss, v_auc = self.eval_model()
-                if epoch % 100 == 0:
-                    self.logger.info('TRAIN Epoch: {} Valid Loss: {:.4}'.format(
-                        epoch, v_loss))
-                    self.logger.info('TRAIN Epoch: {} Valid AUC:  {:.4}'.format(
-                        epoch, v_auc))
+            if epoch % 100 == 0 and validate:
+                with torch.no_grad():
+                    self.model.eval()
+                    v_loss, v_auc = self.exec_core(dl=self.eval_dl, opt=None)
+                self.logger.info('\tEpoch {}\tValid Loss: {:.4}\tAUC: {:.4}'.format(
+                    epoch, v_loss, v_auc))
                 # eval_loss_list.append(loss)
                 # eval_auc_list.append(auc)
-            # if epoch % 100 == 0:
-            #     logger.log(logging.INFO,
-            #                'EVAL  Epoch: {} Loss: {}'.format(epoch,  loss.mean()))
-            # logger.log(logging.INFO,
-            #             'EVAL  Epoch: {} AUC: {}'.format(epoch, auc))
-            # save_model(config, model, auc,epoch)
-            # save_log(
-            #     config,
-            #     (x, train_loss_list, train_auc_list,
-            #         eval_loss_list, eval_auc_list),
-            #     auc, epoch
-            # )
-            # if auc > bset_eval_auc:
-            #     bset_eval_auc = auc
-            #     report['best_eval_auc'] = bset_eval_auc
-            #     report['best_eval_auc_epoch'] = epoch
-    #       if epoch % 10 == 0:
-    #           x.append(epoch)
-    #           if epoch % 100 == 0:
-    #               logger.log(logging.INFO,
-    #                          f'{timeSince(start_time, epoch / config.epoch_size)} ({epoch} {epoch / config.epoch_size * 100})')
+                # if auc > bset_eval_auc:
+                #     bset_eval_auc = auc
+                #     report['best_eval_auc'] = bset_eval_auc
+                #     report['best_eval_auc_epoch'] = epoch
 
-    #   if config.plot_lc:
-    #       fname = model_fname
-    #       save_learning_curve(x, train_loss_list, train_auc_list,
-    #                           eval_loss_list, eval_auc_list, config)
+            # save_model(config, model, auc, epoch)
+
+            # save_log(config, (x, train_loss_list, train_auc_list,
+            #                   eval_loss_list, eval_auc_list), auc, epoch)
+
+            # save_learning_curve(x, train_loss_list, train_auc_list,
+            #                     eval_loss_list, eval_auc_list, config)
+
+            # logger.info(f'{timeSince(start_time, epoch / config.epoch_size)} ({epoch} {epoch / config.epoch_size * 100})')
 
     def eval_model(self):
         with torch.no_grad():
             self.model.eval()
+            return self.exec_core(dl=self.eval_dl, opt=None)
 
-            val_pred_list = []
-            val_actu_list = []
-            current_eval_loss = []
-            for xseq, yseq in self.eval_dl:
-                out = self.model.loss_batch(xseq, yseq, opt=None)
-                loss = out['loss']
-                current_eval_loss.append(loss.item())
-                val_pred_list.append(out['pred_prob'])
-                yseq_a = np.dot(yseq.cpu().numpy(), np.array([[0], [1]]))  # -> (100, 20, 1)
-                val_actu_list.append(torch.Tensor(yseq_a))
+    def exec_core(self, dl, opt):
+        arr_len = len(dl) if not self.config.debug else 1
+        val_pred_arr = np.zeros(
+            [arr_len, self.config.batch_size * self.config.sequence_size])
+        val_actu_arr = np.zeros(
+            [arr_len, self.config.batch_size * self.config.sequence_size])
+        current_eval_loss = np.zeros(arr_len)
+        for i, (xseq, yseq) in enumerate(dl):
+            out = self.model.loss_batch(xseq, yseq, opt=opt)
+            current_eval_loss[i] = out['loss'].item()
+            val_pred_arr[i] = out['pred_prob'].detach().view(-1).cpu()
+            val_actu_arr[i] = yseq[:, :, 1].view(-1).cpu()
 
-                # stop at first batch if debug
-                if self.config.debug:
-                    break
+            if self.config.debug:
+                break
+        # AUC
+        pred = val_pred_arr.reshape(-1)
+        actu = val_actu_arr.reshape(-1)
+        fpr, tpr, thresholds = metrics.roc_curve(actu, pred, pos_label=1)
+        auc = metrics.auc(fpr, tpr)
 
-            loss = np.array(current_eval_loss)
-
-            # AUC, Recall, F1
-            actu = torch.cat(val_pred_list).view(-1).cpu()
-            pred = torch.cat(val_actu_list).view(-1).cpu()
-            # AUC
-            fpr, tpr, thresholds = metrics.roc_curve(
-                pred, actu, pos_label=1)
-            auc = metrics.auc(fpr, tpr)
-
-            return loss.mean(), auc
+        return current_eval_loss.mean(), auc
