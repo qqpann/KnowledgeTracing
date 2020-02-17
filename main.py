372845ddc21e1fb5062cbc0e8818521434ff0731
Qiushi Pan <qiu.gits@gmail.com>
2020-02-18 01:38:52 +0900

    Refactor: format run.py; exit single config

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     if config.is_file():
         print(f'python run.py {str(config)}')
         os.system(f'python run.py {str(config)}')
+        sys.exit(0)  # 0: successful termination.
     os.system(f'ls {str(config)}')
     for cfg in config.glob('*.json'):
         print(f'python run.py {str(cfg)}')
