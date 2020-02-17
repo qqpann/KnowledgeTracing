fb33e4cc172a2929c2ae64e806a38cbaf30f3159
Qiushi Pan <qiu.gits@gmail.com>
2020-02-18 01:15:40 +0900

    ADD: multiple experiments

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from pathlib import Path
+
+if __name__ == '__main__':
+    config = Path(sys.argv[1])
+    assert config.exists()
+    if config.is_file():
+        print(f'python run.py {str(config)}')
+        os.system(f'python run.py {str(config)}')
+    os.system(f'ls {str(config)}')
+    for cfg in config.glob('*.json'):
+        print(f'python run.py {str(cfg)}')
+        os.system(f'python run.py {str(cfg)}')
