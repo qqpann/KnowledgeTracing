c48e9263a03b5d91637d3e27abb2b70b948d9818
Qiushi Pan <qiu.gits@gmail.com>
2020-04-04 20:49:31 +0900

    Refactor: delete old sh file; format file

--- tmp_experiments.sh
+++
@@ -1,4 +0,0 @@
- python main.py --config config/1023_adjust_seqlen_NEL.ini
- python main.py --config config/1023_adjust_seqlen_NER.ini
- python main.py --config config/1023_adjust_seqlen_NES.ini
- python main.py --config config/1023_adjust_seqlen_NEW.ini
